c4edb2aa6402b7591c73b511177896ea4d7d78bf
vanstorm9 <antlowhur@yahoo.com>
2018-07-22 10:33:44 -0400

    Adding bullet feature

--- app.js
+++ app.js
@@ -67,6 +67,46 @@ var Player = function(id){
 	return self;
 }
 
+var Bullet = function(angle){
+	var self = Entity();
+	self.id = Math.random();
+	self.spdX = Math.cos(angle/180*Math.PI)*10;
+	self.spdY = Math.sin(angle/180*Math.PI)*10;
+
+	self.timer = 0;
+	self.toRemove = false;
+	var super_update = self.update;
+	self.update = function(){
+		if(self.timer++ > 100)
+			self.toRemove = true;
+		super_update();
+	}
+	Bullet.list[self.id] = self;
+	return self;
+}
+
+Bullet.list = {};
+
+Bullet.update = function(){
+
+	if(Math.random() < 0.1){
+		Bullet(Math.random()*360);
+	}
+
+	var pack = [];
+	for(var i in Bullet.list){
+		var bullet = Bullet.list[i];
+		bullet.update();
+
+		pack.push({
+			x:bullet.x,
+			y:bullet.y,
+		});
+
+	}
+	return pack;	
+}
+
 Player.list = {};
 Player.onConnect = function(socket){
 
@@ -83,6 +123,7 @@ Player.onConnect = function(socket){
 			player.pressingDown = data.state;
 	});
 }
+
 Player.onDisconnect = function(socket){
 	delete Player.list[socket.id];
 }
@@ -124,7 +165,11 @@ io.sockets.on('connection',function(socket){
 
 
 setInterval(function(){
-	var pack = Player.update();
+
+	var pack = {
+		player: Player.update(),
+		bullet: Bullet.update(),
+	}
 	for(var i in SOCKET_LIST){
 		var socket = SOCKET_LIST[i];
 		socket.emit('newPositions',pack);
